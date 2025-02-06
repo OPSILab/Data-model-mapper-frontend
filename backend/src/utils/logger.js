d0b9ccce6e34a2118c051ef140d8d28d0c9a5f2c
Gabriele <gabriele.percoco@demetrix.it>
2025-02-06 18:32:10 +0100

    tedt deploy

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
+
 const config = require("../../config");
 const winston = require("winston");
 const path = require("path");
