6819857722ea93dcd964ff06d5f0440344faf47a
Gabriele <gabriele.percoco@demetrix.it>
2025-02-06 18:24:56 +0100

    test deploy

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-
 const config = require("../../config");
 const winston = require("winston");
 const path = require("path");
