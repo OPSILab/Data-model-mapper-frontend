9601d82db3c3dd77eebec550e649d95a426102e6
Gabriele <gabriele.percoco@demetrix.it>
2025-02-06 19:26:28 +0100

    tesdt deplot

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-
 const config = require("../../config");
 const winston = require("winston");
 const path = require("path");
